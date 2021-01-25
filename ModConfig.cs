cfa80dcb76d1fb890cd99e74ac77c737b8d1789b
David Camp <Bpendragon@users.noreply.github.com>
2021-01-25 14:26:03 -0800

    Items now display in menu, icons and purcahsing still broken

---
+++ ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestOfQueenOfSauce
+{
+    class ModConfig
+    {
+        public int DaysAfterAiring { get; set; } = 28;
+        public int Price { get; set; } = 3500;
+    }
+}
