d0fee57089a41c686da9ce0e3e5adb780ad46dfb
David Camp <Bpendragon@users.noreply.github.com>
2021-01-24 18:17:25 -0800

    Initial Commit

---
+++ ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StardewModdingAPI;
+
+namespace BestOfQueenOfSauce
+{
+    class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
